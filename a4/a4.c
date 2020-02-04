3977faaf4cfd1599426c4bcce0c243e18634f5de
ruoyun <sun.ruoy@husky.neu.edu>
2020-02-03 16:16:26 -0800

    a4

---
+++ a4/a4.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <string.h>
+
+#include "a4.h"
+
+#define kPrintDebug 1
+
+// Implement the Hand and other functions in here
+Hand* CreateHand() {
+  Hand* h = (Hand*) malloc(sizeof(Hand));
+    h->first_card = NULL;
+    h->num_cards_in_hand=0;
+    return h;
+}
+
+void AddCardToHand(Card *card, Hand *hand) {
+    assert(hand->num_cards_in_hand<kNumCardsInHand);
+    CardNode* card_node = (CardNode*) malloc(sizeof(CardNode));
+    card_node->next_card = NULL;
+    card_node->prev_card = NULL;
+    card_node->this_card = card;
+    if (hand->first_card==NULL) {
+        hand->first_card = card_node;
+    }
+    else {
+        card_node->next_card = hand->first_card;
+        hand->first_card->prev_card = card_node;
+        hand->first_card = card_node;
+    }
+    hand->num_cards_in_hand++;
+}
+
+//?
+Card* RemoveCardFromHand(Card *card, Hand *hand) {
+    if (hand->first_card==NULL){return card;}
+    CardNode* cur = hand->first_card;
+    Card* tmp = cur->this_card;
+    if(&(cur->this_card)==&card) {
+      // hand->first_card = hand->first_card->next_card;
+      //hand->first_card->prev_card = NULL;
+      // free(cur);
+      hand->first_card = hand->first_card->next_card;
+      free(hand->first_card->prev_card);
+      hand->first_card->prev_card = NULL;
+        hand->num_cards_in_hand--;
+        return tmp;
+    }
+    while (cur!=NULL) {
+        if (cur->next_card->this_card->suit == card->suit &&
+            cur->next_card->this_card->name == card->name) {
+            cur->next_card = cur->next_card->next_card;
+            free(cur->next_card->prev_card);
+            if (cur->next_card!=NULL){
+              cur->next_card->prev_card = cur;
+            }
+            hand->num_cards_in_hand--;
+            return card;
+        }
+        cur = cur->next_card;
+    }
+    //Card* cur_card = cur->this_card;
+    // free(cur);//free
+    return card;
+}
+
+int IsHandEmpty(Hand* hand) {
+    if (hand->num_cards_in_hand == 0) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+//?
+void DestroyHand(Hand* hand) {
+    CardNode* cur = hand->first_card;
+    while (cur!=NULL) {
+        free(cur->prev_card);
+        cur = cur->next_card;
+    }
+    free(cur);
+    free(hand);
+}
+
+int IsLegalMove(Hand *hand, Card *leadCard, Card *playedCard) {
+    if (leadCard->suit == playedCard->suit) {
+        return 1;
+    }
+    CardNode* cur = hand->first_card;
+    while (cur!=NULL) {
+        if (cur->this_card->suit == leadCard->suit) {
+            return 0;
+        }
+        cur = cur->next_card;
+    }
+    return 1;
+}
+
+int WhoWon(Card *leadCard, Card *followedCard, Suit trump) {
+    if (leadCard->suit == followedCard->suit) {
+        if (leadCard->value>followedCard->value) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+    else {
+        if (followedCard->suit == trump) {
+            return 0;
+        }
+        return 1;
+    }
+}
+
+void ReturnHandToDeck(Hand *hand, Deck *deck) {
+  CardNode* n = hand->first_card;
+  while (n!=NULL){
+    if(n->prev_card) {
+      PushCardToDeck(n->prev_card->this_card, deck);
+    }
+    n = n->next_card;
+    free(n->prev_card);
+  }
+  free(hand);
+}
---
+++ a4/deck.c
@@ -0,0 +1,94 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "a4.h"
+#define PRINT_DEBUG 1
+#include <assert.h>
+Deck* CreateDeck() {
+    Deck* d = (Deck*) malloc(sizeof(Deck) + kNumCardsInDeck * sizeof(Card) );
+    d->top_card = -1;
+    return d;
+}
+
+Deck* PushCardToDeck(Card* c, Deck* d) {
+  if(d->top_card+1>=kNumCardsInDeck) {return d;}
+    d->cards[++d->top_card] = c;
+    return d;
+}
+
+Card* PeekAtTopCard(Deck* d) {
+  if(d->top_card == -1 || d->top_card>=kNumCardsInDeck) {return NULL;}
+    return d->cards[d->top_card];
+}
+
+Card* PopCardFromDeck(Deck* d) {
+  if (IsDeckEmpty(d)) {return NULL;}
+    Card* popped = d->cards[d->top_card--];
+    return popped;
+}
+
+int IsDeckEmpty(Deck* deck) {
+    if (deck->top_card == -1) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+void DestroyDeck(Deck* deck) {
+  // for (int i=0;i<kNumCardsInDeck;i++) {
+  //    free(deck->cards[i]);
+  //}
+  free(deck->cards);
+    free(deck);
+}
+
+Card* CreateCard(Suit s,Name n) {
+  Card* c = (Card*) malloc(sizeof(Card));
+  c->name = n;
+  c->suit = s;
+  c->value = (int) n;
+}
+
+void DestroyCard(Card* card) {
+  free(card);
+}
+
+//?
+Deck* PopulateDeck() {//Create all the cards and pushes them into the Deck.
+    Deck* d = CreateDeck();
+    int i;
+    for (i=NINE;i<=ACE;i++) {
+        int j;
+        for (j=HEARTS;j<=DIAMONDS;j++) {
+            Card* card = (Card*) malloc(sizeof(Card));
+            card->suit = (enum suit)j;
+            card->value = i;
+            card->name = (enum name)i;
+            PushCardToDeck(card, d);
+        }
+    }
+    return d;
+}
+
+//?Takes all the cards in the deck, rearrange the order, and push the cards back into the Deck.
+void Shuffle(Deck* d) {
+  int len = d->top_card;
+  for (int i=0;i<len;i++){
+    int r = rand()%len;
+    Card* tmp = d->cards[i];
+    d->cards[i] = d->cards[r];
+    d->cards[r] = tmp;
+  }
+}
+
+
+//?Takes a shuffled deck and deals the cards one by one to the each hand.
+void Deal(Deck* d, Hand* hand_1, Hand* hand_2) {
+    for (int i=0;i<5;i++) {
+        Card* card_1 = PopCardFromDeck(d);
+        AddCardToHand(card_1, hand_1);
+        Card* card_2 = PopCardFromDeck(d);
+        AddCardToHand(card_2, hand_2);
+    }
+}
