a792a7f9ccc73377de36aa78fb31a80aa8ccb04f
ruoyun <sun.ruoy@husky.neu.edu>
2020-04-16 16:15:59 -0700

    can work

--- project/MultiServer.c
+++ project/MultiServer.c
@@ -23,6 +23,7 @@
 #include "Util.h"
 
 #define BUFFER_SIZE 1000
+#define N 5
 
 int Cleanup();
 
@@ -56,24 +57,73 @@ void sigint_handler(int sig) {
  */
 int HandleClient(int client_fd, char* query) {
   
-
+  int len;
   // Run query and get responses
-  
+  SearchResultIter iter = FindMovies(docIndex, query);
+  if (iter == NULL) {
+    printf("no result\n");
+    int t = 0;
+    write(client_fd, &t, sizeof(t));
+    char buf_ack[1000];
+    len = read(client_fd, buf_ack, sizeof(buf_ack) - 1);
+    buf_ack[len] = '\0';
+    SendGoodbye(client_fd);
+    close(client_fd);
+    return 0;
+  }
   // Send number of responses
-  
+  int number = NumResultsInIter(iter);
+  char msg[SEARCH_RESULT_LENGTH];
+  sprintf(msg, "%d", number);
+  write(client_fd, &number, sizeof(number));
   // Wait for ACK
-  
+  char buffer_ack[BUFFER_SIZE];
+  len = read(client_fd, buffer_ack, sizeof(buffer_ack) - 1);
+  buffer_ack[len] = '\0';
+  if (CheckAck(buffer_ack) == -1) {
+    printf("not receive ack\n");
+    return -1;
+  }
   // For each response
-  
+
     // Send response
-    
+
     // Wait for ACK
-    
-  
+  SearchResult output = (SearchResult)malloc(sizeof(*output));
+
+  while (SearchResultIterHasMore(iter)) {
+    SearchResultGet(iter, output);
+    // Send response
+    CopyRowFromFile(output, docs, movieSearchResult);
+    write(client_fd, movieSearchResult, strlen(movieSearchResult));
+    // Wait for ACK
+    char buffer_output[BUFFER_SIZE];
+    int len = read(client_fd, buffer_output, sizeof(buffer_output) - 1);
+    buffer_output[len] = '\0';
+    if (CheckAck(buffer_output) == -1) {
+      printf("not receive ack from the client\n");
+      return -1;
+    }
+    SearchResultNext(iter);
+  }
+  SearchResultGet(iter, output);
+  CopyRowFromFile(output, docs, movieSearchResult);
+  write(client_fd, movieSearchResult, strlen(movieSearchResult));
+  char buffer_output_final[BUFFER_SIZE];
+  len = read(client_fd, buffer_output_final, sizeof(buffer_output_final) - 1);
+  buffer_output_final[len] = '\0';
+  if (CheckAck(buffer_output_final) == -1) {
+    printf("not receive ack from the client\n");
+    return -1;
+  }
+
+  free(output);
+  DestroySearchResultIter(iter);
   // Send GOODBYE
-  
+  SendGoodbye(client_fd);
   // close connection.
-  
+  close(client_fd);
+  return 0;
 }
 
 /**
@@ -82,7 +132,60 @@ int HandleClient(int client_fd, char* query) {
 int HandleConnections(int sock_fd, int debug) {
   // Step 5: Accept connection
   // Fork on every connection
-
+  pid_t pid[N];
+  int child_status;
+  int i;
+  for (i = 0; i < N; i++) {
+    if ((pid[i] = fork()) == 0) {
+      if (debug == 1) {
+        sleep(10);
+      }
+      /*
+      printf("Waiting for connection...\n");
+      int client_fd = accept(sock_fd, NULL, NULL);
+      printf("Client connected\n");
+      int r =  SendAck(client_fd);
+      char buffer[BUFFER_SIZE];
+      int len = read(client_fd, buffer, sizeof(buffer) - 1);
+      buffer[len] = '\0';
+      printf("checking goodbye... %s\n", buffer);
+      if (CheckGoodbye(buffer) == -1) {
+        printf("not receive goodbye\n");
+        return -1;
+      }
+      close(client_fd);
+      */
+      while (1) {
+        printf("Waiting for connection...\n");
+        int client_fd = accept(sock_fd, NULL, NULL);
+        printf("Connection made: client_fd=%d\n", client_fd);
+        // Step 6: Read, then write if you want
+        printf("Client connected\n");
+        // Send ACK
+        SendAck(client_fd);
+        // Listen for query
+        // If query is GOODBYE close ocnnection
+        char buf[BUFFER_SIZE];
+        int len = read(client_fd, buf, sizeof(buf) - 1);
+        buf[len] = '\0';
+        printf("checking goodbye... %s \n", buf);
+        if (CheckGoodbye(buf) == 0) {
+          close(client_fd);
+          return 1;
+        }
+        int result = HandleClient(client_fd, buf);
+      }
+      exit(i);
+    }
+  }
+  for (i = 0; i< N;i++) {
+    pid_t wpid = waitpid(pid[i], &child_status, 0);
+    if (WIFEXITED(child_status))
+      printf("Child %d terminated with exit status %d\n"
+               ,wpid, WEXITSTATUS(child_status));
+    else
+      printf("Child %d terminated abnormally\n", wpid);
+  }
   return 0;
 }
 
@@ -121,7 +224,7 @@ int Setup(char *dir) {
     printf("No documents found.\n");
     return 0;
   }
-  
+
   // Index the files
   printf("Parsing and indexing files...\n");
   ParseTheFiles(docs, docIndex);
@@ -162,7 +265,7 @@ int main(int argc, char **argv) {
     printf("No port provided; please include with a -p flag.\n");
     exit(0);
   }
-  
+
   if (dir_to_crawl == NULL) {
     printf("No directory provided; please include with a -f flag.\n");
     exit(0);
@@ -177,16 +280,40 @@ int main(int argc, char **argv) {
   int s;
 
   // Step 1: Get address stuff
-  
+  struct addrinfo hints, *result;
+  memset(&hints, 0, sizeof(struct addrinfo));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+  s = getaddrinfo(NULL, port, &hints, &result);
+  if (s != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+    exit(1);
+  }
 
   // Step 2: Open socket
-  
+  int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 
   // Step 3: Bind socket
-  
+  int yes=1;
+  // lose the pesky "Address already in use" error message
+  if (setsockopt(sock_fd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(yes)) == -1) {
+    perror("setsockopt");
+    exit(1);
+  }
+  if (bind(sock_fd, result->ai_addr, result->ai_addrlen) != 0) {
+    perror("bind()");
+    exit(1);
+  }
 
   // Step 4: Listen on the socket
-  
+  if (listen(sock_fd, 10) != 0) {
+    perror("listen()");
+    exit(1);
+  }
+  freeaddrinfo(result);
+
+  HandleConnections(sock_fd, debug_flag);
 
   // Got Kill signal
   close(sock_fd);
